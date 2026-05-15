586202c440ffa8b21d9af917ea9c1b04ef9e12c2
ignaccortes <ignac.cortesv@duocuc.cl>
2026-05-14 22:30:23 -0400

    Microservicio reporte

---
+++ reporte/src/main/java/com/cafeteria/reporte/ReporteApplication.java
@@ -0,0 +1,16 @@
+package com.cafeteria.reporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+
+public class ReporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReporteApplication.class, args);
+	}
+
+}
