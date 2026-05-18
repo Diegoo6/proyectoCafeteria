fb504b3f7a9207de3cdecaf71f544272ed6225c1
ignaccortes <ignac.cortesv@duocuc.cl>
2026-05-18 03:54:22 -0400

    Venta y reporte actualizados y terminados

--- venta/src/main/java/com/cafeteria/venta/config/FeignConfig.java
+++ venta/src/main/java/com/cafeteria/venta/config/FeignConfig.java
@@ -1,4 +1,4 @@
-package com.cafeteria.venta;
+package com.cafeteria.venta.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
